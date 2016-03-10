b58331fb529418f8f589ac93d5a82045ec9cfe39
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-10 14:35:49 -0800

    refactor emulator into existing chromeApp (work in progress)

--- chromeApp/main.js
+++ chromeApp/main.js
@@ -1,3 +1,12 @@
+/*
+2 Options
+Uncomment either OTPTION 1 or OPTION 2. NOT BOTH!
+*/
+
+
+
+// OPTION 1: Runs index.html which displays local IP
+/*
 chrome.app.runtime.onLaunched.addListener(function(intentData) {
   chrome.app.window.create('index.html', {
       width: 800,
@@ -5,3 +14,26 @@ chrome.app.runtime.onLaunched.addListener(function(intentData) {
   });
   var ipAddresses;
 });
+
+*/
+
+
+
+// OPTION 2: Run's snes9x.html which displays emulator
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+    console.log('quake app launch')
+    
+    //var args = "+skill 3 +map start"
+    var args = "";
+    
+    //chrome.app.window.create('Client/WebQuake.htm' + '?' + encodeURIComponent(args),
+    chrome.app.window.create('snes9x.html',
+                             { defaultWidth: 540,
+                               id:'snes',
+                               defaultHeight: 667  },
+                             function(w) {
+                                 console.log('window created');
+                             })
+
+})
+
