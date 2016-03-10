7d4c50386632495c5a028b69456d301fe7616b7c
franklinshieh <franklinshieh@gmail.com>
2016-03-10 15:28:11 -0800

    Adds in key press and key up events. Adds in arrow pad

--- mobileController/App/Components/ControllerView.js
+++ mobileController/App/Components/ControllerView.js
@@ -21,18 +21,94 @@ class ControllerView extends React.Component {
     super(props);
     this.state = {
       circleButtonSize: undefined,
+      arrowButtonSize: undefined,
     }
   }
 
   componentDidMount() {
-    Orientation.lockToLandscapeRight(); //this will lock the view to Landscape   
+    Orientation.lockToLandscapeRight(); //this will lock the view to Landscape
+
+    //buttons must scale with size of the phone   
     if(Dimensions.get('window').width===375) { //iPhone 6/6s
-      this.setState({circleButtonSize: 62})
+      this.setState({
+      	circleButtonSize: 62,
+      	arrowButtonSize: 52
+      })
     } else if (Dimensions.get('window').width===414) { //iPhone 6+/6s+
-      this.setState({circleButtonSize: 68})
+      this.setState({
+      	circleButtonSize: 68,
+      	arrowButtonSize: 58
+      })
     } else if (Dimensions.get('window').width===320) { //iPhone 5/5s
-      this.setState({circleButtonSize: 53})
+      this.setState({
+      	circleButtonSize: 53,
+      	arrowButtonSize: 44
+      })
     }
+  }
+
+  _APressIn() {
+  	console.log('A pressed')
+  }
+
+  _APressOut() {
+  	console.log('A released')
+  }
+
+  _BPressIn() {
+  	console.log('B pressed')
+  }
+
+  _BPressOut() {
+  	console.log('B released')
+  }
+
+  _XPressIn() {
+  	console.log('X pressed')
+  }
+
+  _XPressOut() {
+  	console.log('X released')
+  }
+
+  _YPressIn() {
+  	console.log('Y pressed')
+  }
+
+  _YPressOut() {
+  	console.log('Y released')
+  }
+
+  _upArrowPressIn() {
+  	console.log('up arrow pressed')
+  }
+
+  _upArrowPressOut() {
+  	console.log('up arrow released')
+  }
+
+  _downArrowPressIn() {
+  	console.log('down arrow pressed')
+  }
+
+  _downArrowPressOut() {
+  	console.log('down arrow released')
+  }
+
+  _rightArrowPressIn() {
+  	console.log('right arrow pressed')
+  }
+
+  _rightArrowPressOut() {
+  	console.log('right arrow released')
+  }
+
+  _leftArrowPressIn() {
+  	console.log('left arrow pressed')
+  }
+
+  _leftArrowPressOut() {
+  	console.log('left arrow released')
   }
 
   render() {
@@ -42,28 +118,47 @@ class ControllerView extends React.Component {
   	    <Image source={require('./Assets/snescontrollercropped.jpg')} style={styles.image}> 
 
           <View style={styles.AButton}> 
-            <TouchableOpacity>
+            <TouchableOpacity onPressIn={this._APressIn.bind(this)} onPressOut={this._APressOut.bind(this)}>
               <IconIon name="record" size={this.state.circleButtonSize} color="#a82530"/>
             </TouchableOpacity>
           </View>
-
           <View style={styles.BButton}> 
-            <TouchableOpacity>
+            <TouchableOpacity onPressIn={this._BPressIn.bind(this)} onPressOut={this._BPressOut.bind(this)}>
               <IconIon name="record" size={this.state.circleButtonSize} color="#d9a04c"/>
             </TouchableOpacity>
           </View>
-
           <View style={styles.XButton}> 
-            <TouchableOpacity>
+            <TouchableOpacity onPressIn={this._XPressIn.bind(this)} onPressOut={this._XPressOut.bind(this)}>
               <IconIon name="record" size={this.state.circleButtonSize} color="#3645ba"/>
             </TouchableOpacity>
           </View>
-
           <View style={styles.YButton}> 
-            <TouchableOpacity>
+            <TouchableOpacity onPressIn={this._YPressIn.bind(this)} onPressOut={this._YPressOut.bind(this)}>
               <IconIon name="record" size={this.state.circleButtonSize} color="#428a43"/>
             </TouchableOpacity>
           </View>
+
+          <View style={styles.upButton}> 
+            <TouchableOpacity underlayColor={'black'} onPressIn={this._upArrowPressIn.bind(this)} onPressOut={this._upArrowPressOut.bind(this)}>
+              <IconIon name="stop" size={this.state.arrowButtonSize} color="transparent"/>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.downButton}> 
+            <TouchableOpacity onPressIn={this._downArrowPressIn.bind(this)} onPressOut={this._downArrowPressOut.bind(this)} >
+              <IconIon name="stop" size={this.state.arrowButtonSize} color="red"/>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.leftButton}> 
+            <TouchableOpacity onPressIn={this._leftArrowPressIn.bind(this)} onPressOut={this._leftArrowPressOut.bind(this)} >
+              <IconIon name="stop" size={this.state.arrowButtonSize} color="red"/>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rightButton}> 
+            <TouchableOpacity onPressIn={this._rightArrowPressIn.bind(this)} onPressOut={this._rightArrowPressOut.bind(this)}>
+              <IconIon name="stop" size={this.state.arrowButtonSize} color="red"/>
+            </TouchableOpacity>
+          </View>
+
 
   	    </Image>
   	  </View>
@@ -99,6 +194,26 @@ var styles = StyleSheet.create({
   	position: 'absolute',
   	top: Dimensions.get('window').width * 0.3875,
   	left: Dimensions.get('window').height * 0.655,
+  },
+  rightButton: {
+  	position: 'absolute',
+  	top: Dimensions.get('window').width * 0.405,
+  	left: Dimensions.get('window').height * 0.24,
+  },
+  downButton: {
+  	position: 'absolute',
+  	top: Dimensions.get('window').width * 0.51,
+  	left: Dimensions.get('window').height * 0.18,
+  },
+  upButton: {
+  	position: 'absolute',
+  	top: Dimensions.get('window').width * 0.30,
+  	left: Dimensions.get('window').height * 0.18,
+  },
+  leftButton: {
+  	position: 'absolute',
+  	top: Dimensions.get('window').width * 0.405,
+  	left: Dimensions.get('window').height * 0.12,
   }
 });
 
