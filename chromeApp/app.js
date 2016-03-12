5ffb09d3df168271bcb79c11e4e298b35fed9943
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-12 15:07:23 -0800

    successful switch to emulator from qrcode screen

--- chromeApp/app.js
+++ chromeApp/app.js
@@ -2,11 +2,12 @@ angular.module('main',[])
 .controller('mainCtrl', function($scope) {
   $scope.ipAddress = 'filler'
   $scope.title = 'TITLEEEE';
-  $scope.playing = true;
+  $scope.emulator = { playing: true}
   $scope.togglePlaying = function() {
     console.log("CLICKEDDDD")
-    $scope.playing = !$scope.playing;
-    console.log($scope.playing)
+    $scope.emulator.playing = true;
+    console.log($scope.emulator.playing)
+    $scope.$apply();
   }
 
 
