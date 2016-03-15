8566665913967a83f4e58abc52eaed090062edaa
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-03-14 20:17:51 -0700

    Add hot module reload middleware to express server

--- webApp/server/server.js
+++ webApp/server/server.js
@@ -1,7 +1,18 @@
 var express = require('express');
+var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var config = require('../webpack.config.js');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleWare = require('webpack-hot-middleware');
 
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath})); // get rid of this middleware for production
+app.use(webpackHotMiddleWare(compiler));
+
+app.use(express.static('./dist'));
 
 app.use(bodyParser.json());
 // app.use(express.static(__dirname + '/../client')); // react index.html
@@ -12,6 +23,9 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use('/', function (req, res) {
+  res.sendFile(path.resolve('client/index.html'));
+});
 
 app.route('/test')
   .get(function(req, res){
@@ -20,10 +34,14 @@ app.route('/test')
 
 
 var port = process.env.PORT || 3000;
-console.log('server running on port ' + port);
 
 // start listening to requests on port 3000
-app.listen(port);
+app.listen(port, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log('Server listening on port ', port);
+});
 
 // export our app for testing
 module.exports = app;
