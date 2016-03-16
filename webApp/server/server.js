d552ed90c85eaf39ac98c7b1d9747c2602306f2c
Joe Miller <joemillervi@gmail.com>
2016-03-15 22:43:48 -0700

    Get video streaming back to client

--- webApp/server/server.js
+++ webApp/server/server.js
@@ -5,19 +5,24 @@ io = require('socket.io')();
 
 var server = require('http').Server(app);
 io.attach(server);
-server.listen(3000);
+server.listen(3000, '0.0.0.0');
 
 var path = require('path');
 
-var CLIENTS = [];
+var currentStreamerid;
+var CLIENTS = {};
 io.on('connection', function(socket) {
-  console.log('a client connected')
-  socket.on('uploadBlob', function(data) {
-    console.log(data);
+  console.log(socket.id)
+  CLIENTS[socket.id] = socket;
+  socket.on('uploadPhoto', function(data) {
+    for (id in CLIENTS) {
+      CLIENTS[id].emit('newFrame', data);
+    }
   })
   socket.on('disconnect', function(){
-    console.log('client disconnected');
-});
+    console.log('client disconnected')
+    delete CLIENTS[socket.id]
+  });
 })
 
 app.use(express.static('./../'))
