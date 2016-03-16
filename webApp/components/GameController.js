c869eacab0c6bd14bb398debfbc0b0fae2cb6561
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-03-16 16:18:18 -0700

    More work on the front end

--- webApp/components/GameController.js
+++ webApp/components/GameController.js
@@ -4,8 +4,8 @@ class GameController extends Component {
   render() {
     return (
       <div className="col s8 height-100">
-        GameController
         <div className="dpad"></div>
+        <div className="start-and-select"></div>
       </div>
     );
   }
