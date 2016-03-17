09f4eafff617e77dd61c8f3814aa8010886e7dd3
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-16 19:19:56 -0700

    add redis.js to clientServer. refactor server.js to serve up static image of emulator on load

--- webApp/clientServer/server.js
+++ webApp/clientServer/server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var redis = require('./redis')();
 var config = require('../webpack.config.js');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleWare = require('webpack-hot-middleware');
+
+process.title = 'crowdmu-web';
 
 var compiler = webpack(config);
 
@@ -15,7 +18,6 @@ app.use(webpackHotMiddleWare(compiler));
 app.use(express.static('./dist'));
 
 app.use(bodyParser.json());
-// app.use(express.static(__dirname + '/../client')); // react index.html
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -27,11 +29,36 @@ app.use('/', function (req, res) {
   res.sendFile(path.resolve('client/index.html'));
 });
 
+var url = process.env.CROWDMU_IO_URL || 'http://localhost:3001';
+app.get('/', function(req, res, next){
+  redis.get('crowdmu:frame', function(err, image){
+    if (err) return next(err);
+    redis.get('crowdmu:connections-total', function(err, count){
+      if (err) return next(err);
+      res.render('index.html', {
+        img: image.toString('base64'),
+        io: url,
+        connections: count
+      });
+    });
+  });
+});
+
+
 app.route('/test')
   .get(function(req, res){
     res.send('Hello World');
 });
 
+app.get('/screenshot.png', function(req, res, next) {
+  redis.get('crowdmu:frame', function(err, image){
+    if (err) return next(err);
+    res.writeHead(200, {
+      'Content-Type':'image/png',
+      'Content-Length': image.length});
+    res.end(image);
+  });
+});
 
 var port = process.env.PORT || 3000;
 
