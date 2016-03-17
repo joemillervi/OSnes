750654bba6362d05a1c8e854be304d9adc6ee6bc
franklinshieh <franklinshieh@gmail.com>
2016-03-16 22:37:30 -0700

    Scales bar chart

--- webApp/components/barChart.js
+++ webApp/components/barChart.js
@@ -9,12 +9,12 @@ import './style.scss';
 const Chart = React.createClass({
   getInitialState: function() {
     return {
-      windowWidth: 100    
+      windowWidth: 40   
     }
   },
 
   componentDidMount: function() {
-    window.setTimeout(()=>this.handleResize(),100)
+    window.setTimeout(()=>this.handleResize(), 100)
     window.addEventListener('resize', this.handleResize);
   },
 
@@ -24,23 +24,22 @@ const Chart = React.createClass({
 
   handleResize: function(e) {
     if(window.innerWidth > 600) {
-      this.setState({windowWidth: window.innerWidth * 0.21});
+      this.setState({windowWidth: ((window.innerWidth) * 2/3 * 1/4  *1.1)}) ;
     } else {
-      this.setState({windowWidth: window.innerWidth * 0.31});
+      this.setState({windowWidth: window.innerWidth * 1/4 * 1.1});
     }
   },
   
   render: function() {
-    console.log(window.innerWidth);
     const data = this.props.data || []
-    const width = this.state.windowWidth-20
+    const width = this.state.windowWidth
     const barHeight = 12;
     const margin = 0;
     const labelSpace = 40;
     
     const x = d3.scale.linear()
                 .domain([0, _.max(data)])
-                .range([0, width])
+                .range([0, width-labelSpace])
     
     const colorScale = d3.scale.category20()
                          .domain(data)
@@ -56,49 +55,41 @@ const Chart = React.createClass({
           transitionLeaveTimeout={3000}
           >
         {
-          data.map((n, i) =>(
-          <g>
-
-            <g 
-
-              transform={`translate(${margin},${barHeight*i})`}>
-              <rect 
-                fill={'white'} 
-                width={labelSpace} 
-                height={barHeight-1}
-               />
-              <text 
-                x={30} 
-                y="9" 
-                dy=".005em"
-                style={{fill:'Black'}}
-              >
-                {'Select'}
-              </text>
+          data.map((n, i) => (
+            <g key={i}>
+              <g 
+                transform={`translate(${margin},${barHeight*i})`}>
+                <rect 
+                  fill={'white'} 
+                  width={labelSpace} 
+                  height={barHeight-1}
+                 />
+                <text 
+                  x="30" 
+                  y="9" 
+                  dy=".005em"
+                  style={{fill:'Black'}}
+                >
+                  {'Select'}
+                </text>
+              </g>
+              <g 
+                transform={`translate(${labelSpace},${barHeight*i})`}>
+                <rect 
+                  fill={colorScale(n)} 
+                  width={x(n)} 
+                  height={barHeight-1}
+                 />
+                <text 
+                  x={x(n)-2} 
+                  y="9" 
+                  dy=".005em"
+                  style={{fill:'white'}}
+                >
+                  {n}
+                </text>
+              </g>
             </g>
-
-            <g 
-
-              transform={`translate(${labelSpace},${barHeight*i})`}>
-              <rect 
-                fill={colorScale(n)} 
-                width={x(n)} 
-                height={barHeight-1}
-               />
-              <text 
-                x={x(n)-3} 
-                y="9" 
-                dy=".005em"
-                style={{fill:'white'}}
-              >
-                {n}
-              </text>
-            </g>
-
-          </g>
-            
-
-
           ))
         }
         </ReactCSSTransitionGroup>
