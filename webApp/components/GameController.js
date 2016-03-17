20498c24ff95795cab03c312ae981f3a9f739a4b
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-03-17 16:06:31 -0700

    Polish game controller UI

--- webApp/components/GameController.js
+++ webApp/components/GameController.js
@@ -6,7 +6,7 @@ import AandB from './AandB';
 class GameController extends Component {
   render() {
     return (
-      <div className="col s8 height-100 signature-color">
+      <div className="col s8 height-100 signature-color rounded-10 border-black">
         <div className="row game-controller-container height-90">
           <DPad />
           <StartAndSelect />
