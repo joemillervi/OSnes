ffa7149cf6f578301603d11174ad463dff7f23c0
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-18 16:55:57 -0700

    fix styling on ChatMessage component

--- webApp/components/ChatMessage.js
+++ webApp/components/ChatMessage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ChatMessage extends Component {
   render() {
     return (
-      <div className="row" style={{marginBottom: 0 + 'px'}}>
+      <div className="row no-bottom-margin">
         <div className="left-algin col s9 push-s3 white-text">{this.props.message.msg}</div>
         <div className="left-align col s3 pull-s9 white-text">{this.props.message.by}</div>
       </div>
