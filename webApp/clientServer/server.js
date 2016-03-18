268f5325bf9093fa03674fff89df9a49e622f6f5
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-18 12:22:43 -0700

    Change index.html to index.ejs. Served up with ioURL as a variable

--- webApp/clientServer/server.js
+++ webApp/clientServer/server.js
@@ -14,15 +14,21 @@ var compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath})); // get rid of this middleware for production
 app.use(webpackHotMiddleWare(compiler));
-
+app.use(bodyParser.json());
 app.use(express.static('./dist'));
 
-app.use(bodyParser.json());
 
-app.use('/', function (req, res) {
-  res.sendFile(path.resolve('client/index.html'));
+var url = process.env.CROWDMU_IO_URL || 'http://localhost:3001';
+
+// Serves index.ejs with socket.io URL included as a variable
+app.set('view engine', 'ejs');
+app.get('/', function(req, res, next){
+  res.render(path.resolve(__dirname + './../client/index'), {
+    ioURL: url
+  });
 });
 
+// Serves a still screenshot of the emulator. Useful for testing connection
 app.get('/screenshot.png', function(req, res, next) {
   redis.get('crowdmu:frame', function(err, image){
     if (err) return next(err);
@@ -32,29 +38,6 @@ app.get('/screenshot.png', function(req, res, next) {
     res.end(image);
   });
 });
-
-// ### Code below servers index.html with image, ioURL, 
-// and connections sent up as variables (work in progress) ###
-
-// var ejs = require('ejs').renderFile
-// app.engine('html', );
-// app.set('view engine', 'html');
-// app.set('views', path.resolve(__dirname + './../client'));
-
-// var url = process.env.CROWDMU_IO_URL || 'http://localhost:3001';
-// app.get('/', function(req, res, next){
-//   redis.get('crowdmu:frame', function(err, image){
-//     if (err) return next(err);
-//     redis.get('crowdmu:connections-total', function(err, count){
-//       if (err) return next(err);
-//       res.render('index', {
-//         img: image.toString('base64'),
-//         io: url,
-//         connections: count
-//       });
-//     });
-//   });
-// });
 
 var port = process.env.CROWDMU_PORT || 3000;
 
