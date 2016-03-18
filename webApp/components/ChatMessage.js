e924b4bab81d7082c7a674e59bcdb0e614cc8d91
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-18 11:18:49 -0700

    fix styling on chats so there's no empty space between rows

--- webApp/components/ChatMessage.js
+++ webApp/components/ChatMessage.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class ChatMessage extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="left-align col s3">{this.props.message.by} : </div>
-        <div className="left-algin col s9">{this.props.message.msg}</div>
+      <div className="row" style={{marginBottom: 0 + 'px'}}>
+        <div className="left-algin col s9 push-s3 white-text">{this.props.message.msg}</div>
+        <div className="left-align col s3 pull-s9 white-text">{this.props.message.by}</div>
       </div>
     );
   }
