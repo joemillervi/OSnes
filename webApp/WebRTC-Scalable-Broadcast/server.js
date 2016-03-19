7f3a689135c952549a7852b0293612bf5a43fd77
root <gstajoe@gmail.com>
2016-03-18 22:59:16 -0400

    Chage port on server

--- webApp/WebRTC-Scalable-Broadcast/server.js
+++ webApp/WebRTC-Scalable-Broadcast/server.js
@@ -43,9 +43,5 @@ var app = require('http').createServer(function (request, response) {
     });
 });
 
-app = app.listen(process.env.PORT || 8888, process.env.IP || "0.0.0.0", function() {
-    var addr = app.address();
-    console.log("Server listening at", addr.address + ":" + addr.port);
-});
-
+app = app.listen(8888)
 require('./WebRTC-Scalable-Broadcast.js')(app);
