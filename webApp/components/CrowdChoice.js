2fc8b5c10f7cd1da9b402719e01dba55ed58f53d
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-19 16:29:02 -0700

    add element for Online count to CrowdChoice component

--- webApp/components/CrowdChoice.js
+++ webApp/components/CrowdChoice.js
@@ -6,7 +6,10 @@ class CrowdChoice extends Component {
   render() {
     return (
       <div className="col s4 height-100">
-        CrowdChoice
+        <div className="row no-bottom-margin">
+          <div className="left-algin col s3 push-s9">Votes</div>
+          <div className="left-align col s9 pull-s3">Online: 4</div>
+        </div>
         <Chart data={[35, 15, 4, 20, 9, 2, 0, 1]}/>
       </div>
     );
