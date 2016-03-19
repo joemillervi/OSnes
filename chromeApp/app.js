45b00e9efb1f4165c07d6b2a6e0b54ab803fb9b8
franklinshieh <franklinshieh@gmail.com>
2016-03-19 13:20:34 -0700

    Adds in titles to controller selection screen and centers

--- chromeApp/app.js
+++ chromeApp/app.js
@@ -2,28 +2,13 @@ angular.module('main',[])
 .controller('mainCtrl', function($scope) {
   $scope.ipAddress = 'filler';
   $scope.ipFound = false;
-  $scope.title = 'TITLEEEE';
   $scope.emulator = { playing: false}
   $scope.togglePlaying = function() {
-    console.log("CLICKEDDDD")
     $scope.emulator.playing = true;
-    console.log($scope.emulator.playing)
     $scope.$apply();
   }
 
   window.togglePlaying = $scope.togglePlaying;
-
-  // For testing keydown events
-  // $('body').on('keydown', function (e) {
-  //   console.log('event triggerd: ', e);
-  // });
-
-  $("#qrCode").css("border-color", "white");
-  $("#qrTitle").text("Scan QR");
-
-  // $("#qrCode").css("border-color", "white");
-  $("#keyboardTitle").text("Click keyboard");
-
 
   document.querySelector('body').addEventListener('keydown', function (e) {
     console.log('da event triggurd: ', e);
@@ -32,14 +17,23 @@ angular.module('main',[])
   chrome.system.network.getNetworkInterfaces(function (ipAddresses) {
     ipAddresses.forEach(function (ipAddress) {
       console.log(ipAddress);
-      if (ipAddress.prefixLength <= 28 && ipAddress.name === "en0") {
+      if (ipAddress.prefixLength < 64 && ipAddress.name === "en0") {
         $scope.title = 'IP FOUND'
 
         $scope.ipAddress = ip4 = ipAddress.address;
         var toQ = $scope.ipAddress + ':' + port;
-              // console.log('toQ');
-              // console.log(toQ);
+
         if($scope.ipFound === false) {
+
+          $("#qrTitle").text("Play on Mobile");
+          $("#qrCode").css("border-color", "white");
+          $("#qrInstructions").text("Scan QR");
+
+          $("#keyboardTitle").text("Play on Desktop");
+          $("#keyboardIcon").css("border-color", "white");
+          $("#keyboardInstructions").text("Click keyboard");
+          $("#keyboardIcon").css("background-color", 'black');
+          $("#keyboardIcon").css("background-image", 'url(' + './keyboard.png' + ')');
           new QRCode(document.getElementById('qrCode'), toQ);
         }
 
