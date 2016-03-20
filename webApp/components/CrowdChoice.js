695a42d030977fc110be75e0d2b0a4e8caa776fc
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-19 17:28:54 -0700

    display total online count

--- webApp/components/CrowdChoice.js
+++ webApp/components/CrowdChoice.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import Chart from './barChart.js'
 
 class CrowdChoice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connections: 0
+    }
+  }
+
+  componentDidMount() {
+    const { socket } = this.props;
+
+    socket.on('connections', (total) => {
+      this.setState({
+        connections: total
+      });
+    });
+  }
+
   render() {
     return (
       <div className="col s4 height-100">
-        CrowdChoice
+        <div className="row no-bottom-margin">
+          <div className="left-algin col s3 push-s9">Votes</div>
+          <div className="left-align col s9 pull-s3">Online: {this.state.connections}</div>
+        </div>
         <Chart data={this.props.votes}/>
       </div>
     );
