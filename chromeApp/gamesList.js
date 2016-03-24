8c30bdff0585ff7393efa77e67e4b9a67fc20f38
franklinshieh <franklinshieh@gmail.com>
2016-03-23 20:46:33 -0700

    Adds in ipfs references to public domain games

--- chromeApp/gamesList.js
+++ chromeApp/gamesList.js
@@ -1,194 +1,131 @@
 var gamesList = [
   {
-    title: 'blah',
-    description: 'Hysteria is a space shooter clone that, while really easy, has something about it that will get you hooked and keep you playing. The music is great, and the graphics and control aren\'t bad either. Try it out.', 
-    link: 'http://gateway.ipfs.io/ipfs/QmeqhHr426TSLZyuEK6xN4fUrF6puuAPV1pHK44SvW2gn2/AIRWOLF.SMC',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
     title: 'Airwolf \'92', 
     description: 'A timed Space Invaders type shooter. Whatever you do, don\'t shoot the yellow balloons.',
-    link: 'http://gateway.ipfs.io/ipfs/QmeqhHr426TSLZyuEK6xN4fUrF6puuAPV1pHK44SvW2gn2/AIRWOLF.SMC',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'Astrohawk', 
-    description: 'A great Asteroids type game.. if you miss flying around and shooting big rocks before they smash into you, this is a game you NEED.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/AIRWOLF.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
     title: 'Bioworm', 
-    description: '',
+    description: 'A cute centipede variation... you chase the bug pieces down corridors to shoot them. Moves a little fast, so tune up your reflexes.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/BIOWORM.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'pokemon', 
-    description: '',
+    title: 'Columns', 
+    description: 'A good clone of the classic puzzler.. and they\'ve added a little something to the background to make sure it gets your attention.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/COLUMNS.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',   
+    title: 'Dynamate', 
+    description: 'This is a cool puzzle game where you move around colored spheres on a playing board. You can solve the puzzles by crashing the spheres into one another.',   
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/dynamate.smc',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Lotto', 
+    description: 'Just as it sounds, you watch the balls roll out of the tumbler and down the shoot.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/LOTTO.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'MazezaM', 
+    description: 'A puzzle game for many platforms now comes to the SNES.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/MazezaM.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Pacman', 
+    description: 'An excellent clone, with graphics that match the original. You\'ll need to be quick, but if you were one of those begging mom for quarters back in the \'80s, you\'re going to love this one.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/PACMAN.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Pong', 
+    description: 'The title says it all.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/PONG.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Rockfall', 
+    description: 'Remember Dig Dug? This will bring it all back to you if you\'ve forgotten. Nice graphics and very enjoyable.. worth a look!',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/ROCKFALL.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Shoot', 
+    description: 'A simple game in which you try to shoot as many enemies as possible in the allotted time. It has basic sound effects as well.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/Shoot.smc',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Sidmania', 
+    description: 'A tribute to the Sid Chip.. this reportedly features 180 C64 music titles.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/SIDMANIA.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Snakeblaster', 
+    description: 'A Space Invaders style game in which you try to shoot down snakes that are bouncing around the screen. There are a few annoying sound bytes, but it is mostly devoid of sound. This is a beta version and you can skip to higher levels by using the Select button.',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/Snakeblaster.smc',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'Zophar\'s Domain 2 Year Anniversary: The Demo', 
+    description: 'This SNES demo was released by Vega to commemorate the 2 year anniversary of Zophar\'s Domain, and I suggest you do download it if you haven\'t already done so, as it\'s pretty cool (after all, it\'s about ZD, isn\'t it?).',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/ZD2YR.SMC',
     console: {
       id: 2,
       name: 'SNES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'A Gameboy Game', 
+    description: 'zzzzzzz description',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/AIRWOLF.SMC',
     console: {
-      id: 2,
-      name: 'SNES'
+      id: 4,
+      name: 'GBA'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
-    console: {
-      id: 2,
-      name: 'SNES'
-    }
-  }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'A NES Game', 
+    description: 'zzzzzzz description',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/AIRWOLF.SMC',
     console: {
       id: 1,
       name: 'NES'
     }
   }, {
-    title: 'blah2222222', 
-    description: '',
+    title: 'A Gameboy Advance Game', 
+    description: 'zzzzzzz description',
+    link: 'http://gateway.ipfs.io/ipfs/QmTuSzxDwpPBFkgiFm1pUsv1hFCQZHGofsdTp712Z4nC91/AIRWOLF.SMC',
     console: {
       id: 3,
       name: 'GB'
-    }
-  }, {
-    title: 'ZZZZZZZZZZ', 
-    description: 'zzzzzzz description',
-    console: {
-      id: 4,
-      name: 'GBA'
     }
   }
 ];
