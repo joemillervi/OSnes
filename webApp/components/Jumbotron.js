650a8f7d383b7b5788e692f808040bf2474edcc7
Joe Miller <joemillervi@gmail.com>
2016-03-28 16:14:43 -0700

    Retrieve if webcam is allowed

--- webApp/components/Jumbotron.js
+++ webApp/components/Jumbotron.js
@@ -4,18 +4,25 @@ class Jumbotron extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showVideo: true,
+      webCamAllowed: false,
+      jumboShown: true,
       outsideStream: null
     }
   }
 
+
+
   toggleVideo() {
+<<<<<<< d889405942592a680ddf28f79efb60e2e57753c4
     this.setState({showVideo: !this.state.showVideo})
     if (!this.state.showVideo) {
       console.log(this.state.showVideo)
       // wait for DOM render
       setTimeout(this.connectVideoToStream.bind(this), 1000)
     };
+=======
+    this.setState({jumboShown: !this.state.jumboShown})
+>>>>>>> Retrieve if webcam is allowed
   }
 
   connectVideoToStream() {
@@ -25,7 +32,136 @@ class Jumbotron extends Component {
     video.play()
   }
 
+  // check if access to webcam is granted. returns bool
+  checkCam() {
+    if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+        // Firefox 38+ seems having support of enumerateDevicesx
+        navigator.enumerateDevices = function(callback) {
+            navigator.mediaDevices.enumerateDevices().then(callback);
+        };
+    }
+
+    var MediaDevices = [];
+    var isHTTPs = location.protocol === 'https:';
+    var canEnumerate = false;
+
+    if (typeof MediaStreamTrack !== 'undefined' && 'getSources' in MediaStreamTrack) {
+        canEnumerate = true;
+    } else if (navigator.mediaDevices && !!navigator.mediaDevices.enumerateDevices) {
+        canEnumerate = true;
+    }
+
+    var hasMicrophone = false;
+    var hasSpeakers = false;
+    var hasWebcam = false;
+
+    var isMicrophoneAlreadyCaptured = false;
+    var isWebcamAlreadyCaptured = false;
+
+    function checkDeviceSupport(callback) {
+        if (!canEnumerate) {
+            return;
+        }
+
+        if (!navigator.enumerateDevices && window.MediaStreamTrack && window.MediaStreamTrack.getSources) {
+            navigator.enumerateDevices = window.MediaStreamTrack.getSources.bind(window.MediaStreamTrack);
+        }
+
+        if (!navigator.enumerateDevices && navigator.enumerateDevices) {
+            navigator.enumerateDevices = navigator.enumerateDevices.bind(navigator);
+        }
+
+        if (!navigator.enumerateDevices) {
+            if (callback) {
+                callback();
+            }
+            return;
+        }
+
+        MediaDevices = [];
+        navigator.enumerateDevices((devices) => {
+            devices.forEach((_device) => {
+                var device = {};
+                for (var d in _device) {
+                    device[d] = _device[d];
+                }
+
+                if (device.kind === 'audio') {
+                    device.kind = 'audioinput';
+                }
+
+                if (device.kind === 'video') {
+                    device.kind = 'videoinput';
+                }
+
+                var skip;
+                MediaDevices.forEach(function(d) {
+                    if (d.id === device.id && d.kind === device.kind) {
+                        skip = true;
+                    }
+                });
+
+                if (skip) {
+                    return;
+                }
+
+                if (!device.deviceId) {
+                    device.deviceId = device.id;
+                }
+
+                if (!device.id) {
+                    device.id = device.deviceId;
+                }
+
+                if (!device.label) {
+                    device.label = 'Please invoke getUserMedia once.';
+                    if (!isHTTPs) {
+                        device.label = 'HTTPs is required to get label of this ' + device.kind + ' device.';
+                    }
+                } else {
+                    if (device.kind === 'videoinput' && !isWebcamAlreadyCaptured) {
+                        isWebcamAlreadyCaptured = true;
+                    }
+
+                    if (device.kind === 'audioinput' && !isMicrophoneAlreadyCaptured) {
+                        isMicrophoneAlreadyCaptured = true;
+                    }
+                }
+
+                if (device.kind === 'audioinput') {
+                    hasMicrophone = true;
+                }
+
+                if (device.kind === 'audiooutput') {
+                    hasSpeakers = true;
+                }
+
+                if (device.kind === 'videoinput') {
+                    hasWebcam = true;
+                }
+
+                // there is no 'videoouput' in the spec.
+
+                MediaDevices.push(device);
+            });
+
+            if (callback) {
+                callback();
+            }
+        });
+    }
+
+    // check for microphone/camera support!
+    checkDeviceSupport(() => {
+      console.log('GOT CALLED', isWebcamAlreadyCaptured)
+      this.setState({webCamAllowed: isWebcamAlreadyCaptured})
+    });
+  }
+
+
   componentDidMount() {
+    this.checkCam() // update the state
+
     navigator.getUserMedia  = navigator.getUserMedia ||
                               navigator.webkitGetUserMedia ||
                               navigator.mozGetUserMedia ||
@@ -122,11 +258,18 @@ class Jumbotron extends Component {
     })
   }
 
+
+  componentWillMount() {
+
+  }
+
   render() {
+
+   console.log('checkstates', this.state.webCamAllowed, this.state.jumboShown)
     return (
       <div>
         <div onClick={this.toggleVideo.bind(this)}>Toggle Jumbotron</div>
-        {this.state.showVideo ? <video className="height-30 margin-4 z-depth-1" id="video-player" width="400" height="300" autoPlay></video> : ''}
+        {this.state.jumboShown && this.state.webCamAllowed ? <Video connectVideoToStream={this.connectVideoToStream.bind(this)}/>: ''}
       </div>
     )
   }
