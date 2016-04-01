30e617f9e8f51ab1797e99de8e3886370b215bc3
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-04-01 10:27:09 -0700

    fix height styling on Sidebar and Video

--- webApp/components/Sidebar.js
+++ webApp/components/Sidebar.js
@@ -5,7 +5,7 @@ import Jumbotron from './Jumbotron';
 class Sidebar extends Component {
   render() {
     return (
-      <div className="col m4 hide-on-small-and-down grey lighten-4 height-88 height-min-510px">
+      <div className="col m4 hide-on-small-and-down grey lighten-4 height-92 height-min-510px">
         <Jumbotron socket={this.props.socket}/>
         <ChatBox socket={this.props.socket} />
       </div>
